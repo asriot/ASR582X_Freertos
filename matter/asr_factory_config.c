66ab48a1ab851f77117307ac410aca8f175bc4b1
weicheng <weichengxu@asrmicro.com>
2023-06-01 15:30:25 +0800

    add BLE fast and slow adv interval support; add rotating device ID support

--- matter/asr_factory_config.c
+++ matter/asr_factory_config.c
@@ -23,7 +23,7 @@
 
 static uint8_t asr_factory_config_para[ASR_MATTER_PARTITION_MAX][8] = {
     "iter-cnt", "    salt", "verifier", "discrimi", "dac-cert", "dacPvkey", "dacPbkey", "pai-cert",
-    "cert-dcl", "vendorNm", "vendorId", "prodctNm", "prodctId", " chip-id", " ftyHash"
+    "cert-dcl", "vendorNm", "vendorId", "prodctNm", "prodctId", "rdid-uid", " chip-id", " ftyHash"
 };
 
 #define ASR_CONFIG_LEN_SIZE 4
